c43c8e7c3b512e28007484539a346831734a84ea
coendewith <coendewith@gmail.com>
2024-04-08 18:19:24 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,108 @@
+import { useState, useEffect } from 'react'
+
+function App() {
+  const [toDos, settoDos] = useState(() => {
+    const saved = localStorage.getItem('toDos');
+    return saved ? JSON.parse(saved) : [];
+  }
+    // { name: 'test1', date: 'Mon, Apr 8, 2 PM' }
+    // , { name: 'test2', date: 'Mon, Apr 8, 2 PM' }
+    // , { name: 'test3', date: 'Mon, Apr 8, 2 PM' }
+  )
+  useEffect(() => {
+    // Store state in localStorage whenever it changes
+    localStorage.setItem('toDos', JSON.stringify(toDos));
+  }, [toDos]);
+
+
+  const [inputValue, setInputValue] = useState('');
+  const DATE_OPTIONS = { weekday: 'short', month: 'short', day: 'numeric', hour: 'numeric' };
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const onEnter = (e) => {
+    if (e.key === 'Enter') {
+      addItem()
+    }
+  }
+  const addItem = () => {
+    if (inputValue != '') {
+      settoDos([...toDos, { name: inputValue, date: (new Date()).toLocaleDateString('en-US', DATE_OPTIONS) }])
+      setInputValue('');
+    }
+  };
+
+  const updateTask = (i) => {
+    // console.log('hello world')
+    const updatedName = prompt('Update the task name:', toDos[i].name)
+    const updatedDate = (new Date()).toLocaleDateString('en-US', DATE_OPTIONS)
+    if (updatedName !== '') {
+      toDos[i].name = updatedName
+      toDos[i].date = updatedDate
+    }
+
+
+    settoDos([...toDos])
+  }
+
+  const deleteItem = (i) => {
+    if (confirm('Are you sure')) {
+      const updatedToDo = toDos.filter((_, index) => index !== i);
+      settoDos(updatedToDo)
+    }
+  };
+
+  const deleteAll = () => {
+    if (confirm('Are you sure')) {
+      settoDos([])
+    }
+  };
+
+  var move = function (array, element, delta) {
+    // Moves an item up or down
+    var index = array.indexOf(element);
+    var newIndex = index + delta;
+    if (newIndex < 0 || newIndex == array.length) return; //Already at the top or bottom.
+    var indexes = [index, newIndex].sort(); //Sort the indixes
+    array.splice(indexes[0], 2, array[indexes[1]], array[indexes[0]]); //Replace from lowest index, two elements, reverting the order
+  };
+
+  const handleMoveup = (i) => {
+    move(toDos, toDos[i], -1);
+    settoDos([...toDos])
+  }
+
+  const handleMoveDown = (i) => {
+    move(toDos, toDos[i], 1);
+    settoDos([...toDos])
+
+  }
+
+
+  return (
+    <>
+      <div className='ToDoList'>
+        <h1>To Do List</h1>
+        <input type="text"
+          value={inputValue}
+          placeholder='Enter Task'
+          onChange={handleInputChange}
+          onKeyDown={onEnter} />
+        <button className='addItem' onClick={addItem} >Add Item</button >
+        <button className='deleteAll' onClick={deleteAll} >Delete All</button >
+        {toDos.map((item, i) =>
+          <ol key={i}> {item.name}
+            <button onClick={() => deleteItem(i)}> ❌ </button>
+            <button onClick={() => updateTask(i)}> ✏️ </button>
+            <button onClick={() => handleMoveup(i)}> ⬆️ </button>
+            <button onClick={() => handleMoveDown(i)}> ⬇️ </button>
+            <p className='start-date'>Last edit: {item.date}</p>
+          </ol>)
+        }
+      </div>
+    </>
+  )
+}
+
+export default App
